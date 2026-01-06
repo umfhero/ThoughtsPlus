9ec7c318fac96768f44e18e8bbe7698d24f6744c
Mauro Fota Marginean <146033349+Disc0nnect3d17@users.noreply.github.com>
2026-01-06 10:19:52 +0000

    Wrap App with DashboardLayoutProvider
    Added DashboardLayoutProvider to the component tree in main.tsx to provide dashboard layout context to the App and its children.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import './styles/index.css'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { NotificationProvider } from './contexts/NotificationContext'
+import { DashboardLayoutProvider } from './contexts/DashboardLayoutContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
             <NotificationProvider>
-                <App />
+                <DashboardLayoutProvider>
+                    <App />
+                </DashboardLayoutProvider>
             </NotificationProvider>
         </ThemeProvider>
     </React.StrictMode>,
